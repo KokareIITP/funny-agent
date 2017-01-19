8278067eab358c627ae4c7f235585eab82679f99
koerino <koerino@gmail.com>
2017-01-19 13:15:45 -0500

    updated function to create 2d array of traversability

--- gridNav/mycreategrid.py
+++ gridNav/mycreategrid.py
@@ -26,28 +26,38 @@ from core import *
 # Creates a grid as a 2D array of True/False values (True =  traversable). Also returns the dimensions of the grid as a (columns, rows) list.
 def myCreateGrid(world, cellsize):
 
-    # find the edges of the screen
-    corners = world.getPoints()
-    maxX = 0
-    maxY = 0
-    for x, y in corners:
-        if x > maxX:
-            maxX = x
-        if y > maxY:
-            maxY = y
+    worldDimensions = world.getDimensions()
+    x = worldDimensions[0]
+    y = worldDimensions[1]
 
     # determine the number of rows and columns needed
-    ### ask on piazza about intersection with boundaries and floor/ceil
-    row = int(math.floor(maxX / cellsize))
-    col = int(math.floor(maxY / cellsize))
-
-    print row, col
+    numRows = int(math.floor(x / cellsize))
+    numCols = int(math.floor(y / cellsize))
 
     # generate 2D array of traversability
-    grid = [[True for i in xrange(col)] for j in xrange(row)]
-    dimensions = (row, col)
+    grid = [[True for i in xrange(numCols)] for j in xrange(numRows)]
+    dimensions = (numRows, numCols)
 
     obstacles = world.getObstacles()
-    ### TO BE COMPLETED
+    for row in xrange(numRows):
+        for col in xrange(numCols):
+            # check for intersections
+            corner1 = (row * cellsize, col * cellsize)
+            corner2 = (row * cellsize, (col + 1) * cellsize)
+            corner3 = ((row + 1) * cellsize, col * cellsize)
+            corner4 = ((row + 1) * cellsize, (col + 1) * cellsize)
+            for obstacle in obstacles:
+                if obstacle.pointInside(corner1) or obstacle.pointInside(corner2) or obstacle.pointInside(corner3) or obstacle.pointInside(corner4):
+                    grid[row][col] = False
+                for point in obstacle.getPoints():
+                    if pointInsidePolygonPoints(point, [corner1, corner2, corner3, corner4]):
+                        grid[row][col] = False
+                for line in obstacle.getLines():
+                    caseA = rayTrace(corner1, corner2, line)
+                    caseB = rayTrace(corner1, corner3, line)
+                    caseC = rayTrace(corner2, corner4, line)
+                    caseD = rayTrace(corner3, corner4, line)
+                    if caseA or caseB or caseC or caseD:
+                        grid[row][col] = False
 
     return grid, dimensions
